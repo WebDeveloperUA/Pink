505a891537272eb8ee90df1913a09c2afb7c7c9b
WebDeveloperUA <petrosem@ukr.net>
2022-01-18 14:57:00 +0200

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,22 @@
+var mainNavToggle = document.querySelector('.main-nav__toggle')
+var mainNavOpened = document.querySelector('.main-nav--opened')
+var mainNavClosed = document.querySelector('.main-nav')
+var mainNav = document.querySelector('.main-nav')
+var pageHeader = document.querySelector('.page-header')
+var siteList = document.querySelector('.site-list')
+
+mainNavToggle.addEventListener("click", function(evt){
+    evt.preventDefault();
+    mainNav.classList.toggle('main-nav--opened')
+    mainNav.classList.toggle('main-nav--closed') 
+
+    if (mainNav.classList.contains('main-nav--opened')){
+        pageHeader.classList.add('page-header--relative')
+        siteList.classList.add('site-list--opened')
+    } else {
+        pageHeader.classList.remove('page-header--relative')
+        siteList.classList.remove('site-list--opened')
+    }
+
+    });
+
